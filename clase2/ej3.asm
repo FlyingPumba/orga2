ddc53afb79a91e73015174d2a775b6e04303e522
Ivan Arcuschin <iarcuschin@gmail.com>
2015-03-19 10:48:56 -0300

    Ej3 clase 2

--- clase2/ej3.asm
+++ clase2/ej3.asm
@@ -1,9 +1,15 @@
+section .data
+    formato: DB '%d %5.2f %s', 10
+    largo EQU $ - formato
 global imprimir_parametros
 extern printf
 section .text
 	imprimir_parametros:
-	mov rax, 1
-	sub rsp, 8
+    push rbp ; align stack
+    mov rbp, rsp ; move rbp to current rsp
+    mov rdx, rsi ; move rsi one slot ahead: rdx <- rsi
+    mov rsi, rdi ; move rdi one slot ahead: rsi <- rdi
+    mov rdi, formato ; put format in rdi
 	call printf
-	add rsp, 8
+    pop rbp ; restore stack
 	ret
--- clase2/ej3.c
+++ clase2/ej3.c
@@ -4,6 +4,6 @@ extern void imprimir_parametros(int, double, char*);
 int main(){
 	int a = 5;
 	double b = 3.2;
-	imprimir_parametros(a, b, "%d %5.2f\n");
+	imprimir_parametros(a, b, "pepito pepe");
 	return 0;
 }
